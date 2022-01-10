5cf7b7c5f62811c3262a8b6800dd063f01fcfb26
ooooo5h <ooooo5h91@gmail.com>
2022-01-10 16:38:37 +0900

    회원가입 API 실행 준비

---
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/SignUpActivity.kt
@@ -0,0 +1,17 @@
+package com.neppplus.pythonapipractice_20220110
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_sign_up.*
+
+class SignUpActivity : BaseActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_up)
+
+        btnSubmit.setOnClickListener {
+
+
+        }
+    }
+}
