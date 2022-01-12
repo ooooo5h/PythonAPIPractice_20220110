43520bc9929fc2cb7bec3512dd2cdba148c53f26
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 12:52:11 +0900

    파이썬으로 : API 만들러

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
@@ -2,12 +2,21 @@ package com.neppplus.pythonapipractice_20220110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_edit_contact.*
 
 class EditContactActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_edit_contact)
 
-        btn
+        btnSave.setOnClickListener { 
+            
+            val inputName = edtName.text.toString()
+            val inputPhoneNume = edtPhoneNum.text.toString()
+            val inputMemo = edtMemo.text.toString()
+            
+//            API 호출 날릴 기능이 없네? API주소도 없네. 파이썬으로 이동
+            apiList.get
+        }
     }
 }
