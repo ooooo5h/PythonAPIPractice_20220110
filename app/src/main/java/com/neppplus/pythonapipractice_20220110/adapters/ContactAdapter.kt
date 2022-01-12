605c59277084b8dbdfbf82de2137326905ede0b2
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 11:53:07 +0900

    연락처 어댑터 클래스 추가했고, 실제 데이터 반영은 아직 안했음

---
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/adapters/ContactAdapter.kt
@@ -0,0 +1,51 @@
+package com.neppplus.pythonapipractice_20220110.adapters
+
+import android.content.Context
+import android.content.Intent
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.neppplus.pythonapipractice_20220110.R
+import com.neppplus.pythonapipractice_20220110.models.ContactData
+import org.w3c.dom.Text
+
+
+class ContactAdapter(
+    val mContext: Context,
+    val mList: List<ContactData>,
+) : RecyclerView.Adapter<ContactAdapter.MyViewHolder>() {
+
+    inner class MyViewHolder(val row: View) : RecyclerView.ViewHolder(row) {
+
+        fun bind(data: ContactData) {
+
+            }
+
+        }
+
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+
+        val row = LayoutInflater.from(mContext).inflate(R.layout.contact_list_item, parent, false)
+
+        return MyViewHolder(row)
+
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+
+
+        holder.bind(mList[position])
+
+    }
+
+    override fun getItemCount(): Int {
+
+        return mList.size
+
+    }
+
+}
