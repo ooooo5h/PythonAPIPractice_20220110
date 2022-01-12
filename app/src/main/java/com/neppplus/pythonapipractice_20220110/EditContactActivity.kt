dd11dfcc9d67b03c17fb12cf3bd1ecd8e5b79e3e
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 13:14:36 +0900

    로그인 시 받아낸 사용자 id값을 기기에 저장해서 연락처 등록시 저장된 id값을 조회해서 사용하도록 활용했음   - 서버에서 토큰값을 못내려주니까, 토큰을 대체하는 용도로 임시활용중(user:id)

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
@@ -1,8 +1,12 @@
 package com.neppplus.pythonapipractice_20220110
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.neppplus.pythonapipractice_20220110.models.BasicResponse
+import com.neppplus.pythonapipractice_20220110.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_edit_contact.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class EditContactActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,11 +16,24 @@ class EditContactActivity : BaseActivity() {
         btnSave.setOnClickListener { 
             
             val inputName = edtName.text.toString()
-            val inputPhoneNume = edtPhoneNum.text.toString()
+            val inputPhoneNum = edtPhoneNum.text.toString()
             val inputMemo = edtMemo.text.toString()
             
 //            API 호출 날릴 기능이 없네? API주소도 없네. 파이썬으로 이동
-            apiList.getRequestAddContact()
+            apiList.getRequestAddContact(ContextUtil.getUserId(mContext), inputName, inputPhoneNum, inputMemo).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+            })
         }
     }
 }
---
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/utils/ContextUtil.kt
@@ -0,0 +1,25 @@
+package com.neppplus.pythonapipractice_20220110.utils
+
+import android.content.Context
+
+class ContextUtil {
+
+    companion object {
+
+        private val prefName = "PythonAPIPracticePref"
+        private val USER_ID = "USER_ID"
+
+        fun setUserId(context: Context, userId : Int) {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            pref.edit().putInt(USER_ID, userId).apply()
+        }
+
+        fun getUserId(context: Context) : Int {
+
+            val pref = context.getSharedPreferences(prefName, Context.MODE_PRIVATE)
+            return pref.getInt(USER_ID, -1)
+        }
+
+    }
+}
