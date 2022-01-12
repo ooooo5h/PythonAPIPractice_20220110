a2181456ae63c6eb1205f65a66be4c72c00ba5ec
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 11:36:28 +0900

    실제 API를 활용해서, 연락처 추가하는 기능을 완료했음

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/EditContactActivity.kt
@@ -2,10 +2,13 @@ package com.neppplus.pythonapipractice_20220110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
 import com.neppplus.pythonapipractice_20220110.models.BasicResponse
 import com.neppplus.pythonapipractice_20220110.utils.ContextUtil
 import kotlinx.android.synthetic.main.activity_edit_contact.*
+import retrofit2.Call
 import retrofit2.Callback
+import retrofit2.Response
 
 class EditContactActivity : BaseActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -18,7 +21,25 @@ class EditContactActivity : BaseActivity() {
             val inputPhoneNum = edtPhoneNum.text.toString()
             val inputMemo = edtMemo.text.toString()
 
-            apiList.getRequestAddContact(ContextUtil.getUserId(mContext), inputName, inputPhoneNum, inputMemo)
+            apiList.getRequestAddContact(ContextUtil.getUserId(mContext), inputName, inputPhoneNum, inputMemo).enqueue(object : Callback<BasicResponse>{
+                override fun onResponse(
+                    call: Call<BasicResponse>,
+                    response: Response<BasicResponse>
+                ) {
+                    if (response.isSuccessful){
+
+                        Toast.makeText(mContext, "연락처 추가 성공", Toast.LENGTH_SHORT).show()
+                        finish()
+                    }
+
+                }
+
+                override fun onFailure(call: Call<BasicResponse>, t: Throwable) {
+
+                }
+
+
+            })
 
         }
     }
