b09c4be5fd02170c2d8a7c8fe16bff148fdcf9e8
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 14:23:02 +0900

    연락처 목록을 조회는 API 호출까지

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/api/APIList.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/api/APIList.kt
@@ -28,4 +28,9 @@ interface APIList {
         @Query("memo") memo : String,
     ) : Call<BasicResponse>
 
+    @GET("/get_contacts")
+    fun getRequestContactList(
+        @Query("user_id") userId : Int,
+    ) :Call<BasicResponse>
+
 }
