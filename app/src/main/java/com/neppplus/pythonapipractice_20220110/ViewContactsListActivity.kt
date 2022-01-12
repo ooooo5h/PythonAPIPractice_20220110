e0094fa18110f4b92dcac481139e946770271ba2
ooooo5h <ooooo5h91@gmail.com>
2022-01-12 14:26:28 +0900

    목록을 받아서 리싸이클러뷰에 연동(실제 데이터 반영은 아직)

--- app/src/main/java/com/neppplus/pythonapipractice_20220110/ViewContactsListActivity.kt
+++ app/src/main/java/com/neppplus/pythonapipractice_20220110/ViewContactsListActivity.kt
@@ -2,21 +2,30 @@ package com.neppplus.pythonapipractice_20220110
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.recyclerview.widget.LinearLayoutManager
+import com.neppplus.pythonapipractice_20220110.adapters.ContactAdapter
 import com.neppplus.pythonapipractice_20220110.models.BasicResponse
 import com.neppplus.pythonapipractice_20220110.models.ContactData
 import com.neppplus.pythonapipractice_20220110.utils.ContextUtil
+import kotlinx.android.synthetic.main.activity_view_contacts_list.*
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
 class ViewContactsListActivity : BaseActivity() {
 
-    val mContactList = ArrayList<ContactData>(
+    val mContactList = ArrayList<ContactData>()
 
-    )
+    lateinit var mAdapter : ContactAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_view_contacts_list)
+
+        mAdapter = ContactAdapter(mContext, mContactList)
+        contactRecyclerView.adapter = mAdapter
+        contactRecyclerView.layoutManager = LinearLayoutManager(mContext)
+
 
         apiList.getRequestContactList(ContextUtil.getUserId(mContext)).enqueue(object : Callback<BasicResponse>{
             override fun onResponse(call: Call<BasicResponse>, response: Response<BasicResponse>) {
@@ -24,6 +33,8 @@ class ViewContactsListActivity : BaseActivity() {
                 if (response.isSuccessful) {
                     val br = response.body()!!
                     mContactList.addAll(br.data.contacts)
+
+                    mAdapter.notifyDataSetChanged()
                 }
 
 
